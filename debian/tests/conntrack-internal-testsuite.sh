7dab55cceede4e9e9acf6c1a3713cd8eabc4b9eb
Arturo Borrero Gonzalez <arturo@debian.org>
2016-12-07 09:40:08 +0100

    d/tests/: only run internal testsuites in newer kernels
    Trying to run the conntrack-tools internal testsuites in olders kernels
    may result in modules not existing and other fails.
    
    So, only run the testsuite in kernel >= 4.x.
    
    Reference:
     https://ci.debian.net/data/packages/unstable/amd64/c/conntrack-tools/20161206_010402.autopkgtest.log.gz
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo@debian.org>

--- debian/tests/conntrack-internal-testsuite.sh
+++ debian/tests/conntrack-internal-testsuite.sh
@@ -4,6 +4,12 @@
 
 set -e
 
+# The testsuite requires kernel at least 4.x
+if [ "$(uname -r | cut -d. -f1)" -lt 4 ] ; then
+	echo "W: this testsuite is likely to produce many fails because of old kernel, ending now"
+	exit 0
+fi
+
 cd tests/conntrack
 bash -e ./run-test.sh
 exit $?
